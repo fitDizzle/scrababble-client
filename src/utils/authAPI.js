dc7876f8d437f6078d865eb7bbd8adf3dc7666e7
fitDizzle <markfitnessaz@gmail.com>
2022-04-13 16:36:47 -0700

    commented out authRefreshAction from app.js temporarily to avoid heroku errors

--- src/utils/authAPI.js
+++ src/utils/authAPI.js
@@ -1,17 +1,17 @@
 import axios from "axios"
 
 const baseUrl = "http://localhost:8000"
-const herokuDB = ""
+const herokuDB = "postgres://rgsfmkdctjvykl:af3b2530cd791b741a38e290e0"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     login: async function(body){
-        return await axios.post(baseUrl + "/auth/login", body)
+        return await axios.post(herokuDB + "/auth/login", body)
     },
     register: async function(body){
-        return await axios.post(baseUrl + "/auth/register", body)
+        return await axios.post(herokuDB + "/auth/register", body)
     },
     getUser: async function(token){
-        return await axios.get(baseUrl + "/auth/get-user/" + token)
+        return await axios.get(herokuDB + "/auth/get-user/" + token)
     }
 }
