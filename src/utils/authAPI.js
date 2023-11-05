793b06ec67a3ba0c6116eef9ecdaf14f97693295
Mark Clark <mark.clark@gusto.com>
2023-11-04 19:26:40 -0700

    fix double /

--- src/utils/authAPI.js
+++ src/utils/authAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const baseUrl = "http://localhost:8000"
-const herokuDB = "https://scrababble-game-server-941f3c890750.herokuapp.com/"
+const herokuDB = "https://scrababble-game-server-941f3c890750.herokuapp.com"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
